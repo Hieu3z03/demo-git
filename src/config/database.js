723a984733b9a4c714c62b5dabd26101edf90ff2
LeHieu <hieukdgvt@gmail.com>
2024-08-10 19:45:36 +0700

    mongoose db

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,37 @@
-//test connection
-const mysql = require('mysql2/promise');
+const mongoose = require("mongoose");
 require('dotenv').config();
-const connection = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    password: process.env.DB_PASSWORD,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+// });
+var dbState = [{
+    value: 0,
+    label: "disconnected"
+},
+{
+    value: 1,
+    label: "connected"
+},
+{
+    value: 2,
+    label: "connecting"
+},
+{
+    value: 3,
+    label: "disconnecting"
+}];
 
-});
+const connection = async () => {
+    try {
+        const options = {
+            user: process.env.DB_USER,
+            pass: process.env.DB_PASSWORD,
+            dbName: process.env.DB_NAME
+        } 
+        await mongoose.connect(process.env.DB_HOST, options);
+        const state = Number(mongoose.connection.readyState);
+        console.log(dbState.find(f => f.value == state).label, "to db"); // connected to db
+    } catch (error) {
+        console.log("Error: ", error);
+    }
+}
+
 
 module.exports = connection
--- src/server.js
+++ src/server.js
@@ -2,18 +2,38 @@ require('dotenv').config();
 const express = require('express');
 const configViewEngine = require('./config/viewEngine');
 const webBrower = require('./routes/web');
+const connection = require('./config/database');
+const mongoose = require('mongoose');
+
+
 const app = express();
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
 
 app.use(express.json()); //utilizes the body-parser package
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 configViewEngine(app);
 
 app.use('/', webBrower);
 
 
-app.listen(port, hostname, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+const kittySchema = new mongoose.Schema({
+    name: String
+});
+const Kitten = mongoose.model('Kitten', kittySchema);
+const silence = new Kitten({ name: 'Hieudz' });
+silence.save();
+
+
+//test connection
+(async () => {
+    try {
+        await connection();
+        app.listen(port, hostname, () => {
+            console.log(`Example app listening on port ${port}`)
+        })
+    } catch (error) {
+        console.log("Error: ", error);
+    }
+})()
